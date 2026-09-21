db67cf05dd8ed38e424559052dcc95a4441b01df
Dounamba Traoré <dounamba@example.com>
2026-09-21 16:23:06 +0000

    hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Products from "./components/Products";
